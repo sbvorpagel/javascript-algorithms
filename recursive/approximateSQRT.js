66ad00f33c1522b6e8df3d76008d288a2bcbd131
sbvorpagel <sbvorpagel@gmail.com>
2018-03-08 21:40:33 -0300

    Added approximateSQRT.

---
+++ recursive/approximateSQRT.js
@@ -0,0 +1,20 @@
+const aSQRT = (num, precision, intervalA, intervalB) => {
+	if (intervalA === undefined) {
+		intervalA = 0;
+	}
+	if (intervalB === undefined) {
+		intervalB = num;
+	}
+	const m = (intervalA + intervalB) / 2;
+	if ((intervalB - intervalA) <= precision) {
+		return intervalA;
+	}
+	if ((m*m) > num) {
+		return aSQRT(num, precision, intervalA, m);
+	} else {
+		return aSQRT(num, precision, m, intervalB);
+	}
+}
+
+// console.log(aSQRT(10, 0.00001)); // 3.1622695922851562
+// console.log(aSQRT(25, 0.00001)); // 4.999995231628418
